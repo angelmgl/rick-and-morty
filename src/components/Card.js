2ac0da80d7e90168919ab451ba9cd0b885257032
Angel Mgl <angelemegeele@gmail.com>
2021-03-03 19:54:37 -0300

    Initial Setup

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+    state = {
+        characters: []
+    }
+
+    render() {
+        return(
+            <div className="card">
+                
+            </div>
+        );
+    }
+}
